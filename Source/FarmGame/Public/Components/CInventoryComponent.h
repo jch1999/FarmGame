3b7f06138986cc56aed1c87679446f4046312a4c
정찬호_JCH <elvpfhhrm@gmail.com>
2025-02-13 14:39:48 +0900

    0213 변경사항 커밋
    주석 제거

--- Source/FarmGame/Public/Components/CInventoryComponent.h
+++ Source/FarmGame/Public/Components/CInventoryComponent.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -13,16 +11,13 @@ class FARMGAME_API UCInventoryComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UCInventoryComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+//	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 		
 };
